e6cce76a9bd74b030a54236707fec9ac3b9ecc4e
Ranul <ranul.s.pathirana@gmail.com>
2022-10-09 14:15:49 +0530

    making the admin portal and log in's

--- venv/E_chan/channeling_app/models.py
+++ venv/E_chan/channeling_app/models.py
@@ -1,3 +1,21 @@
+from datetime import date
+from tokenize import Name
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    Name = models.CharField(max_length=40)
+    Specialty = models.CharField(max_length=40)
+    Reg_number = models.IntegerField()
+
+class Patient(models.Model):
+    Name = models.CharField(max_length=60)
+    Sex = models.CharField(max_length=10)
+    Age = models.IntegerField()
+    ContactNo = models.IntegerField(null=True)
+
+class Appoinment(models.Model):
+    doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    Patient=models.ForeignKey(Patient,on_delete=models.CASCADE)
+    Date_time=models.DateTimeField()
