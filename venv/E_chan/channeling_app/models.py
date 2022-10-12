489c78eaa6b59799bb7326494d7d431c661d7a43
PathiranaKPRS <111476257+PathiranaKPRS@users.noreply.github.com>
2022-10-12 22:33:12 +0530

    removing patient model

--- venv/E_chan/channeling_app/models.py
+++ venv/E_chan/channeling_app/models.py
@@ -6,11 +6,7 @@ from django.db import models
 # Create your models here.
 
 
-class Patient(models.Model):
-    Name = models.CharField(max_length=60)
-    Sex = models.CharField(max_length=10)
-    Age = models.IntegerField()
-    ContactNo = models.IntegerField(null=True)
+
 
 class Appoinment(models.Model):
     doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
